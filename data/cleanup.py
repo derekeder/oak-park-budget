8e79a3558ec8e4c63c18c2aaba39e3492e80f641
Derek Eder <derek.eder@gmail.com>
2021-03-01 20:19:28 -0600

    beginning refactor of cleanup.py, adds 2017 budget file, background psd

--- data/cleanup.py
+++ data/cleanup.py
@@ -3,26 +3,32 @@ import csv
 # link on Fund, Account, Department & Program ID
 
 def cleanup():
+    
+    # 2021 budget
     f = open('raw/2021 Adopted Budget - VOP - Data2018-2021.csv', 'r')
     reader = csv.DictReader(f)
     all_rows = []
     for row in reader:
         for k,v in row.items():
-            if 'Estimates' in k or 'Actuals' in k:
-                v = v.replace('$', '').replace(',','')
-                try:
-                    int(v)
-                    v=int(v)*-1
-                except:
-                    v = 0
-            row[k] = v
+            row[k] = process_row(k,v)
         all_rows.append(row)
     outp = open('final/oak_park_budget_cleaned.csv', 'w')
+    
     writer = csv.DictWriter(outp, row.keys(), quoting=csv.QUOTE_ALL)
     writer.writeheader()
     writer.writerows(all_rows)
     f.close()
     outp.close()
+
+def process_row(k,v):
+    if 'Estimates' in k or 'Actuals' in k:
+        v = v.replace('$', '').replace(',','')
+        try:
+            int(v)
+            v=int(v)*-1
+        except:
+            v = 0
+    return v
 
 if __name__ == "__main__":
     cleanup()
