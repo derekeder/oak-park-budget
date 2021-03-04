5a2d5983f4fad733241b7c7cb1ece98e0dd59ae8
Derek Eder <derek.eder@gmail.com>
2021-03-03 21:27:43 -0600

    behold the power of unique keys

--- data/cleanup.py
+++ data/cleanup.py
@@ -27,13 +27,20 @@ def cleanup():
     budget_2019 = 'raw/2019 Adopted Budget - VOP - Data2016-2017.csv'
     budget_2017 = 'raw/2017 Adopted Budget - Data2013-2015.csv'
     
-    all_rows = []
+    all_rows = {}
     # start with 2021 budget
     print('importing 2021 budget ...')
     with open(budget_2021) as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
-            all_rows.append(row)
+            unique_key = create_unique_key(row)
+            if unique_key not in all_rows:
+                all_rows[unique_key] = {}
+            for k, v in row.items():
+                if k in ['Actuals 2018', 'Actuals 2019', 'Estimates 2020', 'Estimates 2021']:
+                    all_rows[unique_key][k] = set_or_add(all_rows[unique_key], row, k)
+                else:
+                    all_rows[unique_key][k] = v
     
     print ('imported %s rows' % len(all_rows))
     
@@ -49,7 +56,8 @@ def cleanup():
     
     print('cleaning values ...')
     # loop through the result and fill in blanks with zeroes
-    for row in all_rows:
+    rows_to_print = []
+    for unique_key, row in all_rows.items():
         # note - second stop parameter is not inclusive in the range
         for year in range(2013, 2020): 
             row['Actuals %s' % year] = row.get('Actuals %s' % year, '0')
@@ -59,6 +67,8 @@ def cleanup():
         for k,v in row.items():
             if 'Estimates' in k or 'Actuals' in k:
                 row[k] = invert_cell(process_cell(v))
+        
+        rows_to_print.append(row)
 
     print('testing sums ...')
     correct_sums = {
@@ -74,7 +84,7 @@ def cleanup():
     }
 
     for k, v in correct_sums.items():
-        summed_value = sum_value(all_rows, k)
+        summed_value = sum_value(rows_to_print, k)
         if summed_value == v:
             print('%s: %s : sum is correct' % (k,v))
         else:
@@ -85,40 +95,35 @@ def cleanup():
     outp = open('final/oak_park_budget_cleaned.csv', 'w')
     writer = csv.DictWriter(outp, fieldnames=fieldnames, quoting=csv.QUOTE_ALL)
     writer.writeheader()
-    writer.writerows(all_rows)
+    writer.writerows(rows_to_print)
     outp.close()
 
 def append_budget(filepath, keys, all_rows):
+    rows_import = {}
+    count = 0
+    unique_key_sets = []
+
+    # read in the flie to a dict
     with open(filepath) as csvfile:
         reader = csv.DictReader(csvfile)
-        rows_import = []
-        count = 0
         for row in reader:
-            for main_row in all_rows:
-                if match_rows(row, main_row):
-                    row['imported'] = True
-                    for k in keys:
-                        main_row[k] = set_or_add(main_row, row, k)
-                    count = count + 1
-            rows_import.append(row)
-
-        print ('matched %s rows' % count)
-
-        # import the rows we didn't match
-        count = 0
-        for row in rows_import:
-            if 'imported' not in row.keys():
-                count = count + 1
-                all_rows.append(row)
-
-        print ('imported %s additional rows' % count) 
-        print ('rows parsed: %s' % len(rows_import)) 
+            unique_key = create_unique_key(row)
+            if unique_key not in all_rows:
+                all_rows[unique_key] = {}
+            for k, v in row.items():
+                if k in keys:
+                    all_rows[unique_key][k] = set_or_add(all_rows[unique_key], row, k)
+                else:
+                    all_rows[unique_key][k] = v
 
 def sum_value(l, k):
     val = 0
     for row in l:
         val += row[k]
     return val
+
+def create_unique_key(a):
+    return ('%s-%s-%s-%s' %(a['Fund ID'],a['Department ID'],a['Program ID'],a['Account ID']))
 
 def match_rows(a, b):
     if  a['Fund ID'] == b['Fund ID'] and a['Department ID'] == b['Department ID'] and a['Program ID'] == b['Program ID'] and a['Account ID'] == b['Account ID']:
@@ -138,7 +143,6 @@ def process_cell(v):
     if isinstance(v, str): 
         v = v.replace('$', '').replace(',','')
     try:
-        int(v)
         v=int(v)
     except:
         v = 0
